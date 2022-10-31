786debce51b536d84b7ab7cdddcd5fc3edebeb00
Abhinav Paliwal <55943450+urbangabbar@users.noreply.github.com>
2022-10-31 21:09:38 +0530

    Created POST and get api's for User

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
+// This is how we import packages in JS file
 const express = require("express");
 const bodyParser = require("body-parser");
 
-var jsonParser = bodyParser.json();
-
-const app = express();
 const port = 3000;
 
 const users = [];
 
-app.get("/users", (req, res) => {
-    return res.send(users).sendStatus(200)
+// Using express import create an instance of app
+const app = express();
+
+var jsonParser = bodyParser.json();
+
+// app provide multiple methods for HTTP verbs we can add URL and a handler.
+// first param is the URL we want to accept request on, Next param is a callback function to handle request.
+app.get("/api/users", jsonParser, (req, res) => {
+  res.send(users);
 });
 
-app.post("/users", jsonParser, (req, res) => {
-  users.push(req.body);
-  return res.sendStatus(200)
+app.post("/api/users", jsonParser, (req, res) => {
+  users.push({ ...req.body, id: Math.floor(Math.random() * 1000000000) });
+  res.sendStatus(200);
 });
 
-// app.put('/users', (req, res) => {
-
-// })
-
-// app.delete('/users', (req, res) => {
-
-// })
-
+// Listen help you start listening to a particular port.
+// PORT is a channel available on machience
+// 8081;3000;8080;4000;
+// ipAdress:8081 / 8080 -> Domain (Proxy) http:google
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`User app is listening on port ${port}`);
 });
